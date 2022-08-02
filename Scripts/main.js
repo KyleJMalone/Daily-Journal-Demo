b39f59313e8e01218817dc84e9de669d4b248fdf
Kyle Malone <Kylejaxonmalone93@gmail.com>
2022-08-01 19:11:30 -0500

    added script to html

---
+++ Scripts/main.js
@@ -0,0 +1,7 @@
+const journalEntry = {
+    id: 2,
+    date: "08/01/2022",
+    concept: "JavaScript",
+    entry: "I finished my Lenoids-Toys project today with no help. I am enjoying learning javascript",
+    mood: "Happy"
+}
